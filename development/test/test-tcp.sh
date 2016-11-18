872595fffce58053f3658d197d57fabff5e5453e
Martin Dold <martin.dold@gmx.net>
2016-11-18 13:58:22 +0100

    test.tcp: added empty bash script to be used for testing TCP connection

---
+++ development/test/test-tcp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+red='\e[0;31m'
+blue='\e[0;34m'
+green='\e[0;32m'
+clean='\e[0m' # No Color
+
+
+
+# ---- start of script ----
+
+echo -e "${green}=============================${clean}"
+echo -e "${green} SmartMug TCP test script    ${clean}"
+echo -e "${green}=============================${clean}"
+echo -e ""
+
