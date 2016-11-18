ae82e9a37903c122469ec306b1f331cfbb2e0e64
Martin Dold <martin.dold@gmx.net>
2016-11-18 15:10:13 +0100

    arduino: added tcp_close() and tcp_send(); updated docu

--- development/src/microcontroller/arduino/smartmug/tcp.ino
+++ development/src/microcontroller/arduino/smartmug/tcp.ino
@@ -5,8 +5,11 @@
 #include <ESP8266WiFi.h>
 
 /* === Global defines === */
+
+/*! @brief Remote TCP port to connect to. */
 const uint16_t  remote_port = 8080;
-const char     *remote_host = "192.168.2.184"; // ip or dns
+/*! @brief Remote host to connect to. Provided either as IP address or URL. */
+const char     *remote_host = "192.168.2.184";
 
 /* === Global variables === */
 
@@ -17,6 +20,9 @@ WiFiClient client;
 
 /* === Public API functions starting here === */
 
+/*!
+ * @brief Setup function of this ino sketch to setup TCP socket for operation.
+ */
 void tcp_setup()
 {
   Serial.println("\n###");
@@ -36,13 +42,51 @@ void tcp_setup()
       return;
   }
   Serial.println("  Connected.");
+}
+
+/*!
+ * @brief Loop function of this ino sketch to run TCP connection.
+ */
+void tcp_loop()
+{
 
 }
 
 
-void tcp_loop()
+/*!
+ * @brief Closes a TCP connection.
+ */
+void tcp_close()
 {
+  Serial.println("Closing TCP connection");
+  client.stop();
+}
 
+
+/*!
+ * @brief Sends data through established TCP connection.
+ */
+void tcp_send(const uint8_t *data, uint16_t len)
+{
+  Serial.print("TCP_send(): ");
+
+  if (client.connected())
+  {
+    for (int i = 0; i < len; ++i)
+    {
+      // Make debug print of data first.
+      Serial.print(data[i]);
+
+      // Now actually send data through TCP socket.
+      client.write(data[i]);
+    }
+  }
+  else
+  {
+     Serial.println("cannot send data. No connection.");
+  }
+
+  Serial.println("");
 }
 
 /* === Local utility functions starting here === */
