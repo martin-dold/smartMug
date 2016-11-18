3fe027f54f8818ffec66a5cbb674e754a58e171d
Martin Dold <martin.dold@gmx.net>
2016-11-18 15:21:20 +0100

    arduino: added transmission of dummy data once a second using tcp_send()

--- development/src/microcontroller/arduino/smartmug/smartmug.ino
+++ development/src/microcontroller/arduino/smartmug/smartmug.ino
@@ -2,6 +2,22 @@
  *  This is the main() sketch of the smartmug project.
  */
 
+/* === Global defines === */
+
+/*! @brief Timeout that defines the period (in milliseconds) when new TCP data is sent. */
+#define TCP_SEND_TIMEOUT_MS   1000
+
+/* === Global variables === */
+
+/*! @brief Timer for sending TCP data periodically. */
+unsigned long tcpSendTimer;
+
+/*! @brief Some dummy data to be send through TCP socket. In ASCII this is 0 ... 5. */
+uint8_t tcpDummyData[] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35};
+
+
+/* === Local/private function prototypes === */
+
 void setup()
 {
   // Setup serial connection for debugging purpose.
@@ -12,9 +28,30 @@ void setup()
 
   // Setup the TCP connection.
   tcp_setup();
+
+  // Start a timer for sending TCP data periodically.
+  tcpSendTimer = millis();
 }
 
 void loop()
 {
+  // Handle WiFi connection.
+  wifi_loop();
+
+  // Handle TCP connection.
+  tcp_loop();
+
+  // Send out TCP data periodically.
+  if (millis() - tcpSendTimer > TCP_SEND_TIMEOUT_MS)
+  {
+    // Restart timer
+    tcpSendTimer = millis();
+
+    // For now just send some dummy data.
+    tcp_send(tcpDummyData, sizeof(tcpDummyData));
+  }
 
 }
+
+
+/* === Local utility functions starting here === */
