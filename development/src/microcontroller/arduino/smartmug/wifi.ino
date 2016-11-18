a56f4d1c3778266d481975ba536b0858043ba709
Martin Dold <martin.dold@gmx.net>
2016-11-18 12:11:04 +0100

    arduino: added wifi_connect() to connect to user defined SSID

--- development/src/microcontroller/arduino/smartmug/wifi.ino
+++ development/src/microcontroller/arduino/smartmug/wifi.ino
@@ -20,6 +20,9 @@ void wifi_setup()
 
   // Firstly scan the wifi to get a feeling for the smartmug environment
   wifi_scan();
+
+  // Secondly, connect to the requested smartmug SSID
+  wifi_connect();
 }
 
 void wifi_loop()
@@ -32,7 +35,7 @@ void wifi_loop()
 /* === Local utility functions starting here === */
 
 /*!
- * @brief Scans for wifi networks and prints the scan results.
+ * @brief Scans for wifi networks, checks for @ref ssid and prints the scan results.
  */
 void wifi_scan()
 {
@@ -98,5 +101,30 @@ void wifi_scan()
   }
 
   Serial.println("");
-}
+}/* wifi_scan() */
+
+
+/*!
+ * @brief Connects to the requested SSID as defined in @ref ssid.
+ */
+void wifi_connect()
+{
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected.");
+  Serial.println("");
+}/* wifi_connect() */
 
