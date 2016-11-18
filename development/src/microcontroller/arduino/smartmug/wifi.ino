70f327fc08f7805f63201d243a2576938e5dc785
Martin Dold <martin.dold@gmx.net>
2016-11-18 12:00:02 +0100

    arduino: wifi_scan() now checks for the SSID to connect to and prints debug info

--- development/src/microcontroller/arduino/smartmug/wifi.ino
+++ development/src/microcontroller/arduino/smartmug/wifi.ino
@@ -4,12 +4,19 @@
 
 #include <ESP8266WiFi.h>
 
+/*! @brief SSID the smartmug shall connect to. */
+const char* ssid     = "smartmug";
+/*! @brief Password of the SSID the smartmug shall connect to. */
+const char* password = "smartmug12345";
+
 
 /* === Public API functions starting here === */
 
 void wifi_setup()
 {
+  Serial.println("###");
   Serial.println("Starting wifi setup.");
+  Serial.println("###");
 
   // Firstly scan the wifi to get a feeling for the smartmug environment
   wifi_scan();
@@ -29,11 +36,15 @@ void wifi_loop()
  */
 void wifi_scan()
 {
+  bool ssidFound = false;
+  int rssiFound = -100;
+
   // Set WiFi to station mode and disconnect from an AP if it was previously connected
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
 
+  Serial.println("");
   Serial.println("Wifi scan start");
 
   // WiFi.scanNetworks will return the number of networks found
@@ -57,9 +68,35 @@ void wifi_scan()
       Serial.print(WiFi.RSSI(i));
       Serial.print(")");
       Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
+
+      // check if we can see the ssid we should connect to
+      if(WiFi.SSID(i) == ssid)
+      {
+        ssidFound = true;
+        rssiFound = WiFi.RSSI(i);
+      }
+
       delay(10);
     }
   }
+  Serial.println("");
+
+  if(ssidFound)
+  {
+    Serial.print("We found the SSID to connect to: ");
+    Serial.print(ssid);
+    Serial.print(" (RSSI: ");
+    Serial.print(rssiFound);
+    Serial.println(")");
+  }
+  else
+  {
+    Serial.print("ERROR: ");
+    Serial.print(ssid);
+    Serial.println("was not found! Stop here.");
+    while(1);
+  }
+
   Serial.println("");
 }
 
