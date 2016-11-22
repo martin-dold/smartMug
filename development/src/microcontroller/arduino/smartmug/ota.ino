dc2d090e2c606e65dc1e4ed703a3852d3eddee89
martin-dold <martin.dold@gmx.net>
2016-11-22 18:53:08 +0100

    arduino: added emtpy ota sketch for over-the-air-update

---
+++ development/src/microcontroller/arduino/smartmug/ota.ino
@@ -0,0 +1,24 @@
+/*
+ *  This is the Over-The-Air-Update sketch of the smartmug project.
+ */
+
+/* === Global defines === */
+
+/* === Global variables === */
+
+/* === Local/private function prototypes === */
+
+/* === Public API functions starting here === */
+
+void ota_setup()
+{
+
+}
+
+
+void ota_loop()
+{
+
+}
+
+/* === Local utility functions starting here === */
