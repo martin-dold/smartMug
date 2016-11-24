c9f84f6aa97d3bfa1e3934601b616e16d819069b
martin-dold <martin.dold@gmx.net>
2016-11-24 16:58:32 +0100

    arduino: added empty multicast DNS sketch

---
+++ development/src/microcontroller/arduino/smartmug/mdns.ino
@@ -0,0 +1,26 @@
+/*
+ *  This is the multicast DNS sketch of the smartmug project.
+ */
+#include <ESP8266mDNS.h>
+
+/* === Global defines === */
+
+/* === Global variables === */
+
+/* === Local/private function prototypes === */
+
+/* === Public API functions starting here === */
+
+void mdns_setup()
+{
+  
+
+}
+
+
+void mdns_loop()
+{
+
+}
+
+/* === Local utility functions starting here === */
