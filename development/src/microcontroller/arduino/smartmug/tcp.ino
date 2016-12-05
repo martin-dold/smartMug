9c36ae466e5208d14f6dd2d56ed9eff0448f89f9
martin-dold <martin.dold@gmx.net>
2016-12-05 20:07:03 +0100

    arduino: kicked out active TCP client connection; only run server port and wait for incomming connections
    This also deletes hardcoded IP address handling.

--- development/src/microcontroller/arduino/smartmug/tcp.ino
+++ development/src/microcontroller/arduino/smartmug/tcp.ino
@@ -14,16 +14,10 @@
 #include <ESP8266WiFi.h>
 
 /* === Global defines === */
-/*! @brief Remote TCP port to connect to. */
-const uint16_t  remote_port = 8080;
-/*! @brief Remote host to connect to. Provided either as IP address or URL. */
-const char     *remote_host = "192.168.2.184";
 /*! @brief Local TCP port to listen for incomming connections. */
 const uint16_t  local_port = 8081;
 
 /* === Global variables === */
-/*! @brief Use and even initialize WiFiClient class to create TCP connections. */
-WiFiClient client;
 /*! @brief Create WiFi server listening on the given port. */
 WiFiServer server(local_port);
 /*! @brief Create WiFi client that handles client connections received through listening server. */
@@ -41,20 +35,6 @@ void tcp_setup()
   Serial.println("\n###");
   Serial.println("Starting TCP setup.");
   Serial.println("###");
-
-  Serial.print("  Connecting to ");
-  Serial.print(remote_host);
-  Serial.print(" on port ");
-  Serial.println(remote_port);
-
-  if (!client.connect(remote_host, remote_port))
-  {
-      Serial.println("  connection failed");
-      Serial.println("  wait 2 sec...");
-      delay(2000);
-      return;
-  }
-  Serial.println("  Connected.");
 
   Serial.println("  Start TCP listener.");
   // Start the server.
@@ -93,10 +73,6 @@ void tcp_loop()
       break;
     }
   }
-
-  // close the connection:
-//  client.stop();
-//  Serial.println("Connection closed.");
 }
 
 
@@ -106,7 +82,7 @@ void tcp_loop()
 void tcp_close()
 {
   Serial.println("Closing TCP connection");
-  client.stop();
+  serverRemote.stop();
 }
 
 
@@ -119,7 +95,7 @@ void tcp_send(const uint8_t *data, uint16_t len)
 {
   Serial.print("TCP_send(): ");
 
-  if (client.connected())
+  if (serverRemote.connected())
   {
     for (int i = 0; i < len; ++i)
     {
@@ -128,7 +104,7 @@ void tcp_send(const uint8_t *data, uint16_t len)
       Serial.print(" ");
 
       // Now actually send data through TCP socket.
-      client.write(data[i]);
+      serverRemote.write(data[i]);
     }
   }
   else
