356041bc572ebc5a342fc7afb67dd49bf26c011e
martin-dold <martin.dold@gmx.net>
2016-12-05 20:39:06 +0100

    arduino/test: start tcp connection to smartmug, for still using hardcoded IP address
    This is a test script, so hardcoded IP is fine for now. Can later be combined
    with mDNS script support.

--- development/test/show-tcp-smartmug-data.sh
+++ development/test/show-tcp-smartmug-data.sh
@@ -5,6 +5,8 @@ green='\e[0;32m'
 clean='\e[0m' # No Color
 
 # IP configs
+REMOTE_PORT_DATA=8081
+REMOTE_IP_ADDR=192.168.5.101
 
 # ---- start of script ----
 
@@ -13,4 +15,10 @@ echo -e "${green} SmartMug TCP test script showing sensor data   ${clean}"
 echo -e "${green}=============================${clean}"
 echo -e ""
 
+echo -e "${green}Test config:${clean}"
+echo -e "Remote TCP port: $REMOTE_PORT_DATA"
+echo -e "Remote TCP port: $REMOTE_IP_ADDR"
+echo -e ""
 
+echo -e "Connecting to remote..."
+nc -n -v -v $REMOTE_IP_ADDR $REMOTE_PORT_DATA
