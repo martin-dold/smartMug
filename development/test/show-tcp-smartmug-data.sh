b0e25a2a3a9c0de171ccb6d4caad3486437de33f
martin-dold <martin.dold@gmx.net>
2016-12-05 20:35:38 +0100

    arduino/test: added new empty script used to display smartmug sensor data

---
+++ development/test/show-tcp-smartmug-data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+red='\e[0;31m'
+blue='\e[0;34m'
+green='\e[0;32m'
+clean='\e[0m' # No Color
+
+# IP configs
+
+# ---- start of script ----
+
+echo -e "${green}=============================${clean}"
+echo -e "${green} SmartMug TCP test script showing sensor data   ${clean}"
+echo -e "${green}=============================${clean}"
+echo -e ""
+
+
