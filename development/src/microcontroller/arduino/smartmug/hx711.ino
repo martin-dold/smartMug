41ef955394779b4af6d2d59762c987fae6835389
martin-dold <martin.dold@gmx.net>
2016-12-06 18:34:16 +0100

    arduino: added empty hx711.ino sketch to smartmug project

---
+++ development/src/microcontroller/arduino/smartmug/hx711.ino
@@ -0,0 +1,37 @@
+/*! @file  hx711.ino
+ *  @brief This is the HX711 sketch of the smartmug project.
+ *
+ *  @defgroup hx711 HX711 scale
+ *  @brief Functions for weight scaling.
+ *
+ *  @addtogroup arduino
+ *  @{
+ *
+ *  @addtogroup hx711
+ *  @{
+ */
+/* === Global defines === */
+
+/* === Global variables === */
+
+/* === Local/private function prototypes === */
+
+/* === Public API functions starting here === */
+
+void hx711_setup()
+{
+
+}
+
+
+void hx711_loop()
+{
+
+}
+
+/* === Local utility functions starting here === */
+
+/*!
+ * @}
+ * @}
+ */
