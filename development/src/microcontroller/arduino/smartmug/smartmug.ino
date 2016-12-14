c5be889ea18dba0e0a183295997a1095052a74fc
martin-dold <martin.dold@gmx.net>
2016-12-14 18:45:23 +0100

    arduino: fixed length byte in sensorData message; restructured main loop a little

--- development/src/microcontroller/arduino/smartmug/smartmug.ino
+++ development/src/microcontroller/arduino/smartmug/smartmug.ino
@@ -22,7 +22,7 @@
 
 /*! @brief Timeout that defines the period (in milliseconds) when new TCP data is sent. */
 #define TCP_SEND_TIMEOUT_MS   1000
-
+/*! @brief Defines if dummy data shall be sent or not. Set to '1' for sending dummy data. */
 #define SEND_DUMMY_DATA       0
 
 /* === Global variables === */
@@ -30,6 +30,7 @@
 /*! @brief Timer for sending TCP data periodically. */
 unsigned long tcpSendTimer;
 
+#if SEND_DUMMY_DATA
 /*! @brief Some dummy smartmug data to be send through TCP socket.
  *
  *  The dummy data follows the first version of the smartmug protocol by sending:
@@ -37,14 +38,21 @@ unsigned long tcpSendTimer;
  *  LEN:   0x01 (One byte of sensor data)
  *  VALUE: 0xXX (Dummy sensor data from 10, 20, 30, ... 100)
  */
-uint8_t tcpDummyData[] = {0x01, 0x01, 0x00, 0x00};
+uint8_t tcpDummyData[] = {0x01, 0x01, 0x00};
+#else
+/*! @brief First weight sensor data package defined by the smartmug group.
+ *
+ *  The data follows the first version of the smartmug protocol by sending:
+ *  TAG:   0x01 (Smartmug sensor data)
+ *  LEN:   0x02 (Two bytes of sensor data)
+ *  VALUE: two byte weight (in g) as unsigned int with MSB first
+ *  EOF:   '\n' (0x0A) to allow easy data reading in Andriod app
+ */
+uint8_t tcpSensorData[] = {0x01, 0x02, 0x00, 0x00, 0x0A};
+#endif
 
-uint8_t tcpSensorData[] = {0x01, 0x01, 0x00, 0x00, 0x0A};
-
+/*! Last weight that was measured. */
 int currentWeight;
-
-bool isLedOn;
-
 
 /* === Local/private function prototypes === */
 
@@ -72,12 +80,11 @@ void setup()
   hx711_setup();
   currentWeight = 0;
 
-  // Start a timer for sending TCP data periodically.
-  tcpSendTimer = millis();
-
   // Setup LEDs for operation
   led_setup();
-  isLedOn = false;
+
+  // Start a timer for sending TCP data periodically.
+  tcpSendTimer = millis();
 
   Serial.println("\n###");
   Serial.println("Firmware setup complete. Entering main loop.");
@@ -107,15 +114,15 @@ void loop()
   // Send out TCP data periodically.
   if (millis() - tcpSendTimer > TCP_SEND_TIMEOUT_MS)
   {
+    // Restart timer
+    tcpSendTimer = millis();
+
+    /* Read sensor data */
     currentWeight = hx711_getWeightAsInt();
     Serial.print("currentWeight: ");
     Serial.println(currentWeight);
 
-    // Restart timer
-    tcpSendTimer = millis();
-
     #if SEND_DUMMY_DATA
-    // For now just send some dummy data.
     tcp_send(tcpDummyData, sizeof(tcpDummyData));
     tcpDummyData[3] += 10;
     if(tcpDummyData[3] == 110)
@@ -123,6 +130,7 @@ void loop()
       tcpDummyData[3] = 0;
     }
     #else
+    /* Set current weight (MSB first) */
     tcpSensorData[2] = highByte(currentWeight);
     tcpSensorData[3] = lowByte(currentWeight);
     tcp_send(tcpSensorData, sizeof(tcpSensorData));
