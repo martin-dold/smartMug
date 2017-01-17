bf5e55e030105e72907c904972a8c00fe3c25f4e
larraitz8 <larraitzo8@hotmail.com>
2017-01-17 18:04:59 +0100

    Guess game updated

--- development/src/app/SmartMug/app/src/main/java/com/example/smartmug/GameGuessDrinking.java
+++ development/src/app/SmartMug/app/src/main/java/com/example/smartmug/GameGuessDrinking.java
@@ -1,6 +1,7 @@
 package com.example.smartmug;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +11,11 @@ import java.util.Random;
 
 public class GameGuessDrinking extends AppCompatActivity {
 
-    private TextView randomNumber, mugBefore,mugAfter;
+    private TextView randomNumber, mugBefore, result, chrono;
     /**
      * mug content before and after drinking
      */
-    int mugBef, mugAfter;
+    int mugBef, mugAft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +24,11 @@ public class GameGuessDrinking extends AppCompatActivity {
         randomNumber = (TextView) findViewById(R.id.txtRandNumb) ;
         mugBefore = (TextView) findViewById(R.id.txtMugCant) ;
         mugBef = MugContent.mMugcontent_raw;
-        //total = (TextView) findViewById(R.id.totalText);
-        mugBefore.setText(mugBef);
+        mugBefore.setText(String.valueOf(mugBef));
+        result = (TextView) findViewById(R.id.txtResultNumb) ;
+        chrono = (TextView) findViewById(R.id.txtChrono) ;
+        mugAft = MugContent.mMugcontent_raw;
+        //result.setText(String.valueOf(r));
 
        // mugAfter = (TextView) findViewById(R.id.mugAfterText) ;
     }
@@ -36,6 +40,28 @@ public class GameGuessDrinking extends AppCompatActivity {
                 int randomInt = randomGenerator.nextInt(100);
                 randomNumber.setText(String.valueOf(randomInt));
                 break;
+            case R.id.startButton:
+                /**
+                 * start the count down
+                 */
+                new CountDownTimer(5000,1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        // chrono.setBase(millisUntilFinished);
+                        chrono.setText("" + millisUntilFinished / 1000);
+                    }
+                    @Override
+                    /**
+                     * when it finish, show the mug content
+                     */
+                    public void onFinish() {
+                        //crono.setText("done!");
+                        mugAft = MugContent.mMugcontent_raw;
+                        int t = mugBef-mugAft;
+                        result.setText(String.valueOf(t));
+                        mugBef = 0;
+                    }
+                }.start();
 
         }
     }
