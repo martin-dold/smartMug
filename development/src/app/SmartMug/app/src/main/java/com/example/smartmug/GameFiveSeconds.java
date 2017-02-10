61c9bd5d09b22b06f08db83ae0479af4e4318a44
larraitz8 <larraitzo8@hotmail.com>
2017-02-10 15:54:02 +0100

    Fixed 5s drinking game behaviour.

--- development/src/app/SmartMug/app/src/main/java/com/example/smartmug/GameFiveSeconds.java
+++ development/src/app/SmartMug/app/src/main/java/com/example/smartmug/GameFiveSeconds.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /*! @file  
  *  @brief This is the five seconds game activity of the smartmug project.
@@ -26,34 +27,38 @@ public class GameFiveSeconds extends AppCompatActivity {
      * Button for starting the chronometer
      */
     Button start;
-    private TextView crono,mugBefore,mugAfter,total;
+    private TextView crono, mugBefore, mugAfter, total;
     /**
      * The content of mug before and after the chrono
      */
-    int mugBef,mugAft;
+    int mugBef, mugAft;
+
+    int[] mugValues = new int[5];
+    int mugValuesIndex;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_five_seconds);
         start = (Button) findViewById(R.id.startButton);
-        crono = (TextView) findViewById(R.id.cronometro) ;
-        mugBefore = (TextView) findViewById(R.id.mugBeforeText) ;
-        mugAfter = (TextView) findViewById(R.id.mugAfterText) ;
+        crono = (TextView) findViewById(R.id.cronometro);
+        mugBefore = (TextView) findViewById(R.id.mugBeforeText);
+        // mugAfter = (TextView) findViewById(R.id.mugAfterText) ;
         start.setEnabled(true);
         mugBef = MugContent.mMugcontent_raw;
         total = (TextView) findViewById(R.id.totalText);
         mugBefore.setText("Mug Content: " + mugBef);
-        mugAfter.setText("Mug Content: " + mugAft);
+        // mugAfter.setText("Mug Content: " + mugAft);
     }
 
-    public void onClick(View arg){
-        switch (arg.getId()){
+    public void onClick(View arg) {
+        switch (arg.getId()) {
             case R.id.startButton:
                 /**
                  * start the count down
                  */
-                new CountDownTimer(5000,1000) {
+                new CountDownTimer(5000, 1000) {
                     @Override
                     public void onTick(long millisUntilFinished) {
                         // chrono.setBase(millisUntilFinished);
@@ -65,12 +70,27 @@ public class GameFiveSeconds extends AppCompatActivity {
                      * when it finish, show the mug content
                      */
                     public void onFinish() {
+                        crono.setText("Measuring done! Calculating result.");
+                    }
+                }.start();
+                new CountDownTimer(9000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+
+                    }
+
+                    @Override
+                    /**
+                     * when it finish, show the mug content
+                     */
+                    public void onFinish() {
                         crono.setText("done!");
                         mugAft = MugContent.mMugcontent_raw;
-                        mugAfter.setText("Mug Content: " + mugAft);
-                        int t = mugBef-mugAft;
-                        total.setText("Total drinked: " +  t);
-                        mugBef = 0;
+                        //mugAfter.setText("Mug Content: " + mugAft);
+                        int t = mugBef - mugAft;
+                        total.setText("Total drinked: " + t);
+                        mugBef = mugAft;
+                        mugBefore.setText("Mug Content: " + mugBef);
                     }
                 }.start();
                 break;
